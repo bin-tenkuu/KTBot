0e70539263fcb668e8569fa8ab7d234d87a17b4f
2938137849 <2938137849@qq.com>
2022-03-15 16:45:21 +0800

    build: 添加注解包

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 	implementation("io.ktor:ktor-client-serialization-jvm:1.6.7")
 	implementation("io.ktor:ktor-client-json:1.6.7")
 	implementation("org.jetbrains.kotlin:kotlin-reflect:1.4.0")
+	implementation("org.jetbrains:annotations:23.0.0")
 //	implementation("org.slf4j:slf4j-api:1.7.35")
 //	api("net.mamoe:mirai-logging-log4j2:2.9.2")
 //	implementation("org.slf4j:slf4j-simple:1.7.35")
