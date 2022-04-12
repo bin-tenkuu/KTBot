babf6c66c4c361f98ce7dd01388bbdd4a8aba0df
2938137849 <2938137849@qq.com>
2022-04-12 09:54:37 +0800

    memeAI更新，加入自动替换词库

--- src/main/kotlin/my/ktbot/plugs/CQBotMSG.kt
+++ src/main/kotlin/my/ktbot/plugs/CQBotMSG.kt
@@ -2,6 +2,7 @@ package my.ktbot.plugs
 
 import my.ktbot.annotation.Plug
 import my.ktbot.annotation.SubPlugs
+import my.ktbot.utils.ReplaceNode
 import my.ktbot.utils.sendAdmin
 import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.event.events.MessageEvent
@@ -68,27 +69,30 @@ object CQBotMSG : SubPlugs {
 		msgLength = 0..50,
 		hidden = true
 	) {
+		@JvmStatic
+		private val replaceNode = ReplaceNode() + mapOf(
+			"不" to "很",
+			"你" to "我",
+			"我" to "你",
+			"有" to "没有",
+			"没有" to "有",
+			"有没有" to "肯定有",
+			"是" to "不是",
+			"不是" to "是",
+			"是不是" to "肯定是",
+			"？" to "!",
+			"?" to "!",
+			"吗" to "",
+		)
+
 		init {
 			canPrivate = false
 		}
 
-		@JvmStatic
-		private val memeList = arrayOf(
-			Regex("(?<!\\\\)不") to "\\\\很",
-			Regex("(?<!\\\\)你") to "\\\\我",
-			Regex("(?<!\\\\)我") to "\\\\你",
-			Regex("(?<![没\\\\])有") to "\\\\没有",
-			Regex("(?<!\\\\)没有") to "\\\\有",
-			Regex("[？?]") to "!",
-			Regex("[\\\\吗]") to "",
-		)
-
 		override suspend fun invoke(event: GroupMessageEvent, result: MatchResult): Message? {
 			if (!event.message.contains(At(event.bot.id))) return null
-			val msg: String = memeList.fold(event.message.filterIsInstance<PlainText>()
-				.joinToString("", transform = PlainText::contentToString)) { str, (r, s) ->
-				r.replace(str, s)
-			}
+			val msg: String = replaceNode.replace(event.message.filterIsInstance<PlainText>()
+				.joinToString("", transform = PlainText::contentToString))
 			return buildMessageChain {
 				+event.message.quote()
 				+event.sender.at()
---
+++ src/main/kotlin/my/ktbot/utils/ReplaceNode.kt
@@ -0,0 +1,67 @@
+package my.ktbot.utils
+
+class ReplaceNode private constructor(
+	private val children: MutableMap<Char, ReplaceNode>,
+) {
+	constructor() : this(HashMap())
+
+	private var replace: String? = null
+
+	operator fun get(key: Char) = children[key]
+	operator fun set(string: String, replace: String) {
+		string.fold(this) { node, c ->
+			node.children.computeIfAbsent(c) { ReplaceNode() }
+		}.replace = replace
+	}
+
+	fun addAll(map: Map<String, String>): ReplaceNode {
+		map.forEach(::set)
+		return this
+	}
+
+	operator fun plus(map: Map<String, String>) = addAll(map)
+
+	fun replace(charSequence: CharSequence): String {
+		var node = this
+		val sb = StringBuilder(charSequence.length)
+		var nodeTmp: ReplaceNode? = null
+		var skip = false
+		for (c in charSequence) {
+			if (skip) {
+				sb.append(c)
+				skip = false
+				continue
+			}
+			if (c == '\\') {
+				if (nodeTmp != null) {
+					sb.append(nodeTmp.replace)
+					nodeTmp = null
+					node = this
+				}
+				skip = true
+				continue
+			}
+			while (true) {
+				val value = node[c]
+				if (value != null) {
+					if (value.replace != null) nodeTmp = value
+					node = value
+					break
+				}
+				if (nodeTmp == null) {
+					sb.append(c)
+					node = this
+					break
+				}
+				sb.append(nodeTmp.replace)
+				nodeTmp = null
+				node = this
+			}
+		}
+		if (nodeTmp != null) {
+			sb.append(nodeTmp.replace)
+		}
+		return sb.toString()
+	}
+
+}
