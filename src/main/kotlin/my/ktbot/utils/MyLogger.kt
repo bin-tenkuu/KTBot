5d309ba1b267d1b324ae12f24a66e5ba322c9618
2938137849 <2938137849@qq.com>
2022-04-21 09:58:22 +0800

    加入logback

--- src/main/kotlin/my/ktbot/utils/MyLogger.kt
+++ src/main/kotlin/my/ktbot/utils/MyLogger.kt
@@ -1,9 +1,10 @@
 package my.ktbot.utils
 
-import net.mamoe.mirai.utils.DirectoryLogger
+import net.mamoe.mirai.utils.LoggerAdapters.asMiraiLogger
 import net.mamoe.mirai.utils.MiraiLogger
-import java.io.File
-import java.time.Duration
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import kotlin.reflect.KClass
 
 /**
  *
@@ -12,16 +13,34 @@ import java.time.Duration
  * @date 2022/1/28
  */
 class MyLogger : MiraiLogger.Factory {
+	companion object : MiraiLogger.Factory {
+		fun getLogger(requester: Class<*>, identity: String? = null): Logger {
+			return LoggerFactory.getLogger(identity ?: requester.simpleName ?: requester.name)
+		}
+
+		fun getLogger(requester: KClass<*>, identity: String? = null): Logger = getLogger(requester.java, identity)
+
+		override fun create(requester: Class<*>, identity: String?): MiraiLogger {
+			// return DirectoryLogger(identity ?: requester.simpleName ?: requester.name, File("./logs"),
+			// 	Duration.ofDays(10).toMillis())
+			return getLogger(requester, identity).asMiraiLogger()
+		}
+
+	}
+
 	override fun create(requester: Class<*>, identity: String?): MiraiLogger {
 		return MyLogger.create(requester, identity)
 	}
 
-	companion object : MiraiLogger.Factory {
-		override fun create(requester: Class<*>, identity: String?): MiraiLogger {
-			return DirectoryLogger(identity ?: requester.simpleName ?: requester.name, File("./logs"),
-				Duration.ofDays(10).toMillis())
-			// return LoggerFactory.getLogger(identity ?: requester.simpleName ?: requester.name).asMiraiLogger()
-		}
+	override fun create(requester: Class<*>): MiraiLogger {
+		return MyLogger.create(requester)
 	}
 
+	override fun create(requester: KClass<*>): MiraiLogger {
+		return MyLogger.create(requester)
+	}
+
+	override fun create(requester: KClass<*>, identity: String?): MiraiLogger {
+		return MyLogger.create(requester, identity)
+	}
 }
