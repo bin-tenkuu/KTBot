2b8dfd1a19360495f93e2ac992febe811dc0ad89
2938137849 <2938137849@qq.com>
2022-05-07 16:25:37 +0800

    feature: 添加更详细的事件触发日志及记录

--- src/main/kotlin/my/ktbot/PluginMain.kt
+++ src/main/kotlin/my/ktbot/PluginMain.kt
@@ -14,6 +14,7 @@ import net.mamoe.mirai.event.*
 import net.mamoe.mirai.event.events.*
 import net.mamoe.mirai.message.data.PlainText
 import net.mamoe.mirai.message.data.isContentEmpty
+import net.mamoe.mirai.utils.MiraiExperimentalApi
 import java.time.Duration
 
 /**
@@ -104,23 +105,27 @@ object PluginMain : KotlinPlugin(
 			logger.info("${bot.nameCardOrNick} 已上线")
 		}
 		subscribeAlways<BotOfflineEvent> {
+			val msg = "${bot.nameCardOrNick} 已下线, 是否重连: ${reconnect}, 原因: ${
+				when (this) {
+					is BotOfflineEvent.Active -> "主动离线, 错误: ${cause}"
+					is BotOfflineEvent.Force -> "被挤下线, title: ${title}, message: ${message}"
+					// is BotOfflineEvent.MsfOffline -> "主动离线, 错误: ${cause}"
+					is BotOfflineEvent.Dropped -> "因网络问题而掉线, 错误: ${cause}"
+					// is BotOfflineEvent.RequireReconnect -> "主动离线, 错误: ${cause}"
+					else -> toString()
+				}
+			}"
 			bots -= bot.id
 			startCounter()
-			logger.info("${bot.nameCardOrNick} 已下线")
+			logger.info(msg)
 		}
 		subscribeAlways<BotReloginEvent> {
-			bots[bot.id] = bot
-			startCounter()
 			logger.info("${bot.nameCardOrNick} 已重新登录")
 		}
 		subscribeAlways<NewFriendRequestEvent> {
-			sendAdmin(
-				"${fromNick}（${
-					fromId
-				}）来自群 ${fromGroup?.name ?: ""}（${
-					fromGroupId
-				}）请求添加好友消息：\n${message}"
-			)
+			val msg = "${fromNick}（${fromId}）来自群 ${fromGroup?.name ?: ""}（${fromGroupId}）请求添加好友消息：\n${message}"
+			logger.info("NewFriendRequestEvent: ${msg}")
+			sendAdmin(msg)
 			if (inviteCount.size <= 10) {
 				inviteCount[fromId] = Unit
 				//自动同意好友申请
@@ -129,7 +134,9 @@ object PluginMain : KotlinPlugin(
 			else reject()
 		}
 		subscribeAlways<BotInvitedJoinGroupRequestEvent> {
-			sendAdmin("${invitorNick}（${invitorId}）邀请加入群 ${groupName}（${groupId}）")
+			val msg = "${invitorNick}（${invitorId}）邀请加入群 ${groupName}（${groupId}）"
+			logger.info("BotInvitedJoinGroupRequestEvent: ${msg}")
+			sendAdmin(msg)
 			Counter.groups[groupId].update {
 				exp = 20.0
 				invited = invitorId
@@ -141,24 +148,44 @@ object PluginMain : KotlinPlugin(
 //			}
 		}
 		subscribeAlways<MemberJoinEvent> {
-			val msg = "@${member.nick} " + when (this) {
-				is MemberJoinEvent.Invite -> "被 @${invitor.nick} 邀请"
-				is MemberJoinEvent.Active -> "欢迎"
-				else -> ""
-			} + "入群"
+			val msg = "@${member.nick} ${
+				when (this) {
+					is MemberJoinEvent.Invite -> "被 @${invitor.nick} 邀请"
+					is MemberJoinEvent.Active -> "欢迎"
+					is MemberJoinEvent.Retrieve -> "恢复群主身份"
+					else -> ""
+				}
+			}入群"
+			logger.info("MemberJoinEvent: ${msg}")
 			try {
 				group.sendMessage(PlainText(msg))
 			} catch (e: Exception) {
 				logger.error(toString(), e)
 				sendAdmin("来自群：${groupId}\n${msg}")
 			}
+		}
+		@OptIn(MiraiExperimentalApi::class)
+		subscribeAlways<BotJoinGroupEvent> {
+			val msg = "bot成功加入群： ${group.name}(${groupId}), 来源：${
+				when (this) {
+					is BotJoinGroupEvent.Invite -> "邀请人： ${invitor.nick}(${invitor.id})"
+					is BotJoinGroupEvent.Active -> "不确定"
+					is BotJoinGroupEvent.Retrieve -> "恢复群主身份"
+					else -> ""
+				}
+			}"
+			logger.info("BotJoinGroupEvent: ${msg}")
+			sendAdmin(msg)
 		}
 		subscribeAlways<MemberLeaveEvent> {
-			val msg = "@${member.nick}(${member.id})" + when (this) {
-				is MemberLeaveEvent.Quit -> "主动离开本群"
-				is MemberLeaveEvent.Kick -> "被 管理员(@${operator?.nick ?: bot.nick}) 踢出本群"
-				else -> "未知方式离开本群"
-			}
+			val msg = "@${member.nick}(${member.id})${
+				when (this) {
+					is MemberLeaveEvent.Quit -> "主动离开本群"
+					is MemberLeaveEvent.Kick -> "被 管理员(@${operator?.nick ?: bot.nick}) 踢出本群"
+					else -> "未知方式离开本群"
+				}
+			}"
+			logger.info("MemberLeaveEvent: ${msg}")
 			try {
 				group.sendMessage(PlainText(msg))
 			} catch (e: Exception) {
@@ -166,29 +193,43 @@ object PluginMain : KotlinPlugin(
 				sendAdmin("来自群：${groupId}\n${msg}")
 			}
 		}
+		@OptIn(MiraiExperimentalApi::class)
 		subscribeAlways<BotLeaveEvent> {
-			sendAdmin("bot被踢出群：${groupId}(${group.name})")
+			val msg = "bot被踢出群：${groupId}(${group.name}), 原因：${
+				when (this) {
+					is BotLeaveEvent.Active -> "主动退出, 有被踢出可能"
+					is BotLeaveEvent.Kick -> "被踢出群"
+					is BotLeaveEvent.Disband -> "群主解散群聊"
+					else -> ""
+				}
+			}"
+			logger.info("BotLeaveEvent: ${msg}")
+			sendAdmin(msg)
 		}
 		subscribeAlways<OtherClientOnlineEvent> {
-			sendAdmin(
-				"""其他客户端上线
+			val msg = """其他客户端上线
 				|设备名称:${client.info.deviceName}
 				|设备类型:${client.info.deviceKind}
 			""".trimMargin()
-			)
+			logger.info("OtherClientOnlineEvent: ${msg}")
+			sendAdmin(msg)
 		}
 		subscribeAlways<OtherClientOnlineEvent> {
-			sendAdmin(
-				"""其他客户端下线
+			val msg = """其他客户端下线
 				|设备名称:${client.info.deviceName}
 				|设备类型:${client.info.deviceKind}
 			""".trimMargin()
-			)
+			logger.info("OtherClientOnlineEvent: ${msg}")
+			sendAdmin(msg)
 		}
 		subscribeAlways<BotMuteEvent> {
+			val msg = "bot被禁言，群: ${group.name}(${groupId}), 操作人: ${operator.nameCardOrNick}(${operator.id})"
+			logger.info("BotMuteEvent: ${msg}")
 			Counter.groups[groupId].update { isBaned = true }
 		}
 		subscribeAlways<BotUnmuteEvent> {
+			val msg = "bot被取消禁言，群: ${group.name}(${groupId}), 操作人: ${operator.nameCardOrNick}(${operator.id})"
+			logger.info("BotUnmuteEvent: ${msg}")
 			Counter.groups[groupId].update { isBaned = false }
 		}
 	}
