f7850063d8b916b56d382e3702a153d6c84f6d12
2938137849 <2938137849@qq.com>
2022-06-01 09:55:13 +0800

    feature: 自动转发新要求2

--- src/main/kotlin/my/ktbot/plugs/BotProxy.kt
+++ src/main/kotlin/my/ktbot/plugs/BotProxy.kt
@@ -1,7 +1,6 @@
 package my.ktbot.plugs
 
 import my.ktbot.annotation.AutoSend
-import my.ktbot.utils.ReplaceNode
 import my.ktbot.utils.get
 import my.miraiplus.annotation.MessageHandle
 import my.miraiplus.annotation.RegexAnn
@@ -14,35 +13,11 @@ import net.mamoe.mirai.event.events.GroupMessageEvent
  *  @version 1.0.0
  */
 object BotProxy {
-	@JvmStatic
-	private val replaceNode = ReplaceNode(false) + mapOf(
-		"：" to " ",
-		":" to " ",
-		"。" to " ",
-		"." to " ",
-		"," to " ",
-		"," to " ",
-		"！" to " ",
-		"!" to " ",
-		"？" to " ",
-		"?" to " ",
-		"、" to " ",
-		"；" to " ",
-		";" to " ",
-
-		"“" to "",
-		"”" to "",
-		"\"" to "",
-		"【" to "",
-		"】" to "",
-		"《" to "",
-		"》" to "",
-		"「" to "",
-		"」" to "",
-	)
 
 	private var groupId: Long? = null
-	private val space = Regex(" {2,}")
+	private val space = Regex("[　\\s]+")
+	private val toSpace = "：:。，.,！!？?、；; ".toSet()
+	private val toDel = "“”\"【】《》「」".toSet()
 
 	@MessageHandle(".c开始转发消息")
 	@RegexAnn("^[.．。]cstart", RegexOption.IGNORE_CASE)
@@ -60,14 +35,20 @@ object BotProxy {
 	}
 
 	@MessageHandle(".c转发具体消息")
-	@RegexAnn("^[.．。]c (?<msg>.*)", RegexOption.IGNORE_CASE)
+	@RegexAnn("^[.．。]c (?<msg>.*)", RegexOption.IGNORE_CASE, RegexOption.MULTILINE)
 	@AutoSend
 	suspend fun cProxy(event: FriendMessageEvent, result: MatchResult): String {
 		val gId = this.groupId ?: return "无具体转发群"
 		val tmp = event.bot.getGroup(gId) ?: return "未找到对应群"
 		val value = result["msg"]?.value ?: return "无匹配消息"
-		val msg = replaceNode.replace(value).trim().replace(space, " ")
-		tmp.sendMessage(msg)
+		val msg = String(value.mapNotNull {
+			if (it in toSpace) return@mapNotNull '　'
+			else if (it in toDel) return@mapNotNull null
+			return@mapNotNull it
+		}.toCharArray()).replace(space, "　").trim()
+		if (msg.isNotEmpty()) {
+			tmp.sendMessage(msg)
+		}
 		return "转发至群：${tmp.name}(${gId})"
 	}
 }
