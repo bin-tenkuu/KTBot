f42093b22e0a3f2563731c28352d04146f98204b
2938137849 <2938137849@qq.com>
2022-06-02 09:13:49 +0800

    feature: 自动调用框架迁移完成

---
+++ src/main/kotlin/my/ktbot/annotation/NeedExp.kt
@@ -0,0 +1,42 @@
+package my.ktbot.annotation
+
+import my.ktbot.database.Gmt.Companion.add
+import my.ktbot.utils.Counter
+import my.miraiplus.Caller
+import my.miraiplus.injector.Injector
+import net.mamoe.mirai.event.events.FriendMessageEvent
+import net.mamoe.mirai.event.events.GroupMessageEvent
+import net.mamoe.mirai.event.events.MessageEvent
+
+/**
+ *  @Date:2022/6/2
+ *  @author bin
+ *  @version 1.0.0
+ */
+annotation class NeedExp(val private: Double, val group: Double) {
+	object Inject : Injector.Message<NeedExp> {
+		override suspend fun doBefore(ann: NeedExp, event: MessageEvent, caller: Caller): Boolean {
+			return when (event) {
+				is FriendMessageEvent -> ann.private > 0
+					|| Counter.members[event.sender.id].exp > 0
+				is GroupMessageEvent -> ann.group > 0
+					|| Counter.groups[event.group.id].exp > 0
+					|| Counter.members[event.sender.id].exp > 0
+				else -> false
+			}
+		}
+
+		override suspend fun doAfter(ann: NeedExp, event: MessageEvent, caller: Caller, result: Any?) {
+			when (event) {
+				is FriendMessageEvent -> {
+					Counter.members[event.sender.id].add(ann.private)
+				}
+				is GroupMessageEvent -> {
+					Counter.groups[event.group.id].add(ann.group)
+					Counter.members[event.sender.id].add(ann.group)
+				}
+				else -> {}
+			}
+		}
+	}
+}
