3cb2d4b79f19267920231506f930f25a8791516d
bin_tenkuu <2938137849@qq.com>
2023-04-25 12:55:27 +0800

    feature: 向postgresql迁移

--- MainBot/src/main/kotlin/my/ktbot/ktor/dao/HisMsg.kt
+++ MainBot/src/main/kotlin/my/ktbot/ktor/dao/HisMsg.kt
@@ -20,18 +20,9 @@ interface HisMsg : Entity<HisMsg> {
     companion object : Entity.Factory<HisMsg>()
 }
 
-open class THisMsg(
-    alias: String? = "hm",
-) : Table<HisMsg>(tableName = "HisMsg", alias = alias, entityClass = HisMsg::class) {
+object THisMsg : Table<HisMsg>(tableName = "HisMsg", entityClass = HisMsg::class) {
     val id: Column<Long> = long("id").bindTo { it.id }.primaryKey()
-
     val type: Column<String> = varchar("type").bindTo { it.type }
-
     val msg: Column<String> = varchar("msg").bindTo { it.msg }
-
     val role: Column<String> = varchar("role").bindTo { it.role }
-
-    override fun aliased(alias: String): THisMsg = THisMsg(alias)
-
-    companion object : THisMsg()
 }
